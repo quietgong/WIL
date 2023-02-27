25f6434dfaf9857b0d3fb91dd2cdad83adeedc7d
LAPTOP-GMU6JBJ8\quiet <quietgong@naver.com>
2023-02-27 10:58:00 +0900

    자바 쇼트서킷

---
+++ basic/ShortCircuit.java
@@ -0,0 +1,11 @@
+package WIL.basic;
+
+public class ShortCircuit {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        int a=10;
+        if(a>1 || arr[100]>=10) // a>1 조건에 의해 arr[100]의 조건은 무시된다.
+            System.out.println("Short Circuit");
+        // 결과 : Short Circuit
+    }
+}
