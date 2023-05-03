3fab1678b877336225e3be3a4cf0c0956c21c1d2
LAPTOP-GMU6JBJ8\quiet <quietgong@naver.com>
2023-05-03 17:15:37 +0900

    순열, 조합

---
+++ algo/Combination.java
@@ -0,0 +1,42 @@
+package WIL.algo;
+
+import java.util.Arrays;
+
+/* Output
+* [1, 2, 3]
+Combination (뽑기만한다.)
+1
+2
+3
+1 2
+1 3
+2 3
+1 2 3
+ */
+public class Combination {
+    public static void combination(int[] arr, boolean[] visited, int start, int depth, int r) {
+        if (depth == r) {
+            for (int i = 0; i < arr.length; i++) {
+                if (visited[i]) System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = start; i < arr.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                combination(arr, visited, i + 1, depth + 1, r);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("Combination");
+        for (int r = 1; r <= arr.length; r++)
+            combination(arr, new boolean[arr.length], 0, 0, r);
+    }
+}
--- algo/Permutation.java
+++ algo/Permutation.java
@@ -1,6 +1,5 @@
 package WIL.algo;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /* Output
@@ -21,17 +20,8 @@ Permutation (뽑은 뒤 줄 세운다.)
 2 3 1
 3 1 2
 3 2 1
-*
-Combination (뽑기만한다.)
-1
-2
-3
-1 2
-1 3
-2 3
-1 2 3
- */
-public class NumberOfCases {
+*/
+public class Permutation {
     public static void permutation(int[] arr, int[] out, boolean[] visited, int depth, int r) {
         if (depth == r) {
             for (int num : out) System.out.print(num + " ");
@@ -47,22 +37,7 @@ public class NumberOfCases {
             }
         }
     }
-    public static void combination(int[] arr, boolean[] visited, int start, int depth, int r) {
-        if (depth == r) {
-            for (int i = 0; i < arr.length; i++) {
-                if (visited[i]) System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-            return;
-        }
-        for (int i = start; i < arr.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                combination(arr, visited, i + 1, depth + 1, r);
-                visited[i] = false;
-            }
-        }
-    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3};
         System.out.println(Arrays.toString(arr));
@@ -70,9 +45,5 @@ public class NumberOfCases {
         System.out.println("Permutation");
         for (int r = 1; r <= arr.length; r++)
             permutation(arr, new int[r], new boolean[arr.length], 0, r);
-
-        System.out.println("Combination");
-        for (int r = 1; r <= arr.length; r++)
-            combination(arr, new boolean[arr.length], 0, 0, r);
     }
 }
